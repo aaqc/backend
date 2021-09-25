0f18b8baef2d72a36410eb3e872d1bbd32fbd6e3
Ismail Sacic <cicas03@hotmail.com>
2021-09-25 21:15:03 +0200

    Added some new api-end points

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from logging import debug
 import uvicorn
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-from typing import Optional
+from typing import List
+
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"Test": "Test"}
+    return {"start": "nice"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/active_connections")
+def active_connections():
+    """Returns how many active-connections with the websocket!
+    
+    Returns:
+        [int]: [Length of manager.active_connections witch is a list]
+    """    
+    return {"active-connections": len(manager.active_connections)}
 
 
 @app.get("/websocket")
@@ -20,13 +26,38 @@ async def get():
     html = open("./app/websocket-example.html", "r").read()
     return HTMLResponse(html)
 
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, debug=True)
