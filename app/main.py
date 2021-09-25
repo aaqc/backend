c027810545cfc5bd4bcdf6d294f71a70563913c9
Alve Svarén <alve@hotmail.se>
2021-09-25 21:18:32 +0000

    Fix problems with container

--- app/main.py
+++ app/main.py
@@ -60,4 +60,4 @@ manager = ConnectionManager()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, debug=True)
+    uvicorn.run("main:app", port=8000)
