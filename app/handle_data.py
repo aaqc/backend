7393b57be7532a2713a5fc25072614eed2bf3954
E. Almqvist <elalmqvist@gmail.com>
2021-09-27 17:31:59 +0200

    Better handle_data implementation

--- app/handle_data.py
+++ app/handle_data.py
@@ -1,3 +1,20 @@
+from connection_manager import ConnectionManager
 from typing import Any, Optional, Union
 from time import time_ns
 import typing
+
+def handle_data(manager: ConnectionManager, t: str) -> tuple[str, Any]:
+    if t == "ping":
+        return "pong", None
+    elif t == "log":
+        return "ack", None
+    elif t == "time":
+        return "time", {"utc_ns": time_ns()}
+    elif t == "who":
+        return "who", {
+            "clients": list(manager.clients.keys()),
+            "providers": list(manager.providers.keys()),
+            "count": len(manager.connections),
+        }
+
+    raise NotImplementedError("Message t is not implemented")
