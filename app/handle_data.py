492cf95bc46609bda0fc092dc3d4930602eef836
E. Almqvist <elalmqvist@gmail.com>
2021-09-27 18:12:49 +0200

    Wrapper function for all api functions

--- app/handle_data.py
+++ app/handle_data.py
@@ -3,18 +3,13 @@ from typing import Any, Optional, Union
 from time import time_ns
 import typing
 
-def handle_data(manager: ConnectionManager, t: str) -> tuple[str, Any]:
-    if t == "ping":
-        return "pong", None
-    elif t == "log":
-        return "ack", None
-    elif t == "time":
-        return "time", {"utc_ns": time_ns()}
-    elif t == "who":
-        return "who", {
-            "clients": list(manager.clients.keys()),
-            "providers": list(manager.providers.keys()),
-            "count": len(manager.connections),
-        }
+from api_functions import functions
 
-    raise NotImplementedError("Message t is not implemented")
+def wrapper_function(manager: ConnectionManager, t: str) -> tuple[str, Any]:
+    if(t not in functions):
+        raise NotImplementedError("Message t is not implemented")
+
+    return functions[t](data={"manager": manager})
+
+def handle_data(manager: ConnectionManager, t: str) -> tuple[str, Any]:
+    return wrapper_function(manager, t)
