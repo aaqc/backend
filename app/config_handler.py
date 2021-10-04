7e14a2485187d6e2a70753f2b28874558ad08cca
E. Almqvist <elalmqvist@gmail.com>
2021-10-04 21:10:04 +0200

    Rename fetch_token to proper name

--- app/config_handler.py
+++ app/config_handler.py
@@ -16,6 +16,6 @@ with open(config_filepath, "r") as stream:
     except yaml.YAMLError as err:
         print(f"YAML Error: {err}")
 
-def fetch_token(api: str) -> str:
+def get_token(api: str) -> str:
     return CONFIG["tokens"][api] 
 
