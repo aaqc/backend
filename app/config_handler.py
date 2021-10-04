1f83b76e759c8ee8cbaadaa372d30741befc5408
E. Almqvist <elalmqvist@gmail.com>
2021-10-04 21:07:48 +0200

    fetch_token

---
+++ app/config_handler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import yaml
+
+CONFIG = {} # config for everything
+config_filepath = "./config.yml"
+
+with open(config_filepath, "r") as stream:
+    try:
+        CONFIG = yaml.safe_load(stream)
+
+    except FileNotFoundError as err:
+        print("Unable to load config.yml. No such file or directory.")
+        raise err
+
+    except yaml.YAMLError as err:
+        print(f"YAML Error: {err}")
+
+def get_token(api: str) -> str:
+    return CONFIG["tokens"][api] or ""
+
