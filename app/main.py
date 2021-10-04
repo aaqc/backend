2ff31d5c807cc2afc480eaf06e34a49990aea398
E. Almqvist <elalmqvist@gmail.com>
2021-10-04 22:43:54 +0200

    Stuff

--- app/main.py
+++ app/main.py
@@ -24,12 +24,12 @@ manager = ConnectionManager()
 
 @app.get("/")
 async def index():
-    return {"isvimbetterthanvscode": "no, but nano is also worse"}
+    return {"hello": "there"}
 
 
 @app.get("/isvimbetterthanvscode")
 async def isvimbetterthanvscode():
-    return "Vim is lim[x->inf] x times worse., but nano is lim[x->inf] x worse than VIM and vscode"
+    return "Vim is lim[x->inf] x times better than everything and vscode is lim[x->inf] x worse than VIM and nano"
 
 
 @app.websocket("/gateway")
@@ -68,26 +68,6 @@ async def hello():
 def active_connections():
     """Returns how many clients and providers are connected to this gateway"""
     return {"count": len(manager.connections)}
-
-
-# @app.get("/websocket")
-# async def get():
-#     html = open("./websocket-example.html", "r").read()
-#     return HTMLResponse(html)
-
-
-# @app.websocket("/ws/{client_id}")
-# async def websocket_endpoint(websocket: WebSocket, client_id: int):
-#     await manager.connect(websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             await manager.send_personal_message(f"You wrote: {data}", websocket)
-#             await manager.broadcast(f"Client #{client_id} says: {data}")
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket)
-#         await manager.broadcast(f"Client #{client_id} left the chat")
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000)
