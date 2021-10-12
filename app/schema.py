1040a8d98d24f8bb932d48b159c8cde48f1d0847
Alve Svarén <alve@hotmail.se>
2021-10-12 10:33:16 +0200

    Merge local stash

--- app/schema.py
+++ app/schema.py
@@ -1,28 +1,48 @@
 from __future__ import annotations
 from pydantic import BaseModel
 from typing import Any, Optional
+from pydantic import BaseModel, validator
+from typing import Any, Optional
+
+from pydantic.networks import validate_email
 
 
 class AAQCBaseModel(BaseModel):
     id: int
 
+
+class AAQCBaseModelOrm(AAQCBaseModel):
     class Config:
         orm_mode = True
 
 
-class User(AAQCBaseModel):
-    id: int
+class BaseUser(AAQCBaseModel):
     username: str
+    email: str
+    full_name: str
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: str
+    password: str
+    full_name: str
+
+
+class User(BaseUser):
     email: str
     full_name: str
     groups: list[Group]  # Group
 
 
-class UserCreate(BaseModel):
+class UserLogin(BaseModel):
     username: str
     email: str
-    full_name: str
     password: str
+
+
+class UserCreate(UserLogin):
+    full_name: str
 
 
 class Group(AAQCBaseModel):
@@ -61,32 +81,3 @@ models: list[Any] = [User, UserCreate, Group, Drone, FlightPath, Waypoint]
 
 for model in models:
     model.update_forward_refs()
-
-
-# class ItemCreate(ItemBase):
-#     pass
-
-
-# class Item(ItemBase):
-#     id: int
-#     owner_id: int
-
-#     class Config:
-#         orm_mode = True
-
-
-# class UserBase(BaseModel):
-#     email: str
-
-
-# class UserCreate(UserBase):
-#     password: str
-
-
-# class User(UserBase):
-#     id: int
-#     is_active: bool
-#     items: List[Item] = []
-
-#     class Config:
-#         orm_mode = True
