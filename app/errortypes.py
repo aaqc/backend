528fe5dd6218c4d953fda52406c5b1b8a349c3e1
E. Almqvist <elalmqvist@gmail.com>
2021-10-16 01:10:57 +0200

    Better error info

--- app/errortypes.py
+++ app/errortypes.py
@@ -2,61 +2,70 @@ from fastapi import HTTPException
 
 
 class API_Error(Exception):
-    def __init__(self, message: str = "API Error", status_code: int = 500):
-        self.message = message
+    def __init__(self, status_code: int = 500, error: str = "api-error", errorString: str = "API Error"):
         self.status_code = status_code
+        self.error = error
+        self.errorString = errorString
 
 
 class GenericError(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "Something went wrong, check logs"
+        self.error = "generic-error"
+        self.errorString = "Something went wrong, check logs"
 
 
 class MessageTypeMissing(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "The type parameter is missing"
         self.status_code = 400
+        self.error = "message-type-missing"
+        self.errorString = "The type parameter is missing"
 
 
 class MessageTypeInvalid(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "The type parameter is of incorrect type"
         self.status_code = 400
+        self.error = "message-type-invalid"
+        self.errorString = "The type parameter is of incorrect type"
 
 
 class APINotImplemented(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "Not implemented"
         self.status_code = 501
+        self.error = "api-not-implemented"
+        self.errorString = "Not implemented"
 
 
 class UserNotFound(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "User not found"
         self.status_code = 404
+        self.error = "user-not-found"
+        self.errorString = "User not found"
 
 
 class AuthFailure(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "Authetication failure"
         self.status_code = 401
+        self.error = "auth-failure"
+        self.errorString = "Authetication failure"
 
 
 class UserCreationFailure(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "User creation failed. Possible duplicate credentials"
         self.status_code = 400
+        self.error = "user-creation-failure"
+        self.errorString = "User creation failed. Possible duplicate credentials"
 
 
 class ThirdPartyError(API_Error):
     def __init__(self, *args, **kwargs):
         super(*args, **kwargs)
-        self.message = "Third-party API or service failed. Nothing we can do :("
         self.status_code = 503
+        self.error = "thid-party-error"
+        self.errorString = "Third-party API or service failed. Nothing we can do :("
